f58c54cf474024d3113ceb65298128b580f03afd
Daniel <danielchacon1699@gmail.com>
2023-01-16 19:40:39 -0600

    weather app done

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a167188c9cmsh6485a9fe2929195p1bae01jsn31a321a78989',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+export const WEATHER_API_KEY = "63819b78dbcfea38550a95093af22381"
